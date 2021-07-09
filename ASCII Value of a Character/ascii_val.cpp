09fce31e5bd42166189cad8ce344f0b8109cf24b
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-07-09 14:03:50 +0530

    Add files via upload

---
+++ ASCII Value of a Character/ascii_val.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"Enter the character : ";
+    cin>>ch;
+
+    cout<<"The ASCII value of character "<<ch<<" is "<<(int)ch;
+
+    return 0;
+}
