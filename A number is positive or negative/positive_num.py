1dc027ec8c093ed82ef7f57558d6b9b8579a9102
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-07-09 13:55:01 +0530

    Add files via upload

---
+++ A number is positive or negative/positive_num.py
@@ -0,0 +1,11 @@
+#WAP in Python to check whether a number is positive or negative
+
+
+num = int(input("Enter the number : "))
+
+if num>0:
+    print("Entered number is positive")
+elif num == 0:
+    print("Entered number is zero")
+else:
+    print("Entered number is negative")
