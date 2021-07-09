5a408e89505ccb46247fde8d3267d56c84da37b9
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-07-09 13:54:37 +0530

    Add files via upload

---
+++ A number is even or odd/even_odd.py
@@ -0,0 +1,9 @@
+#WAP in Python to check whether a number is odd or even
+
+num = int(input("Enter the number : "))
+
+if num%2==0:
+    print("Entered number is Even")
+else:
+    print("Entered number is Odd")
+
