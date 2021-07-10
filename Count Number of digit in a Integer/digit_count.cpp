649747504df1812e8bda245c763805ef954dd4a5
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2021-07-10 08:07:08 +0530

    Add files via upload

---
+++ Count Number of digit in a Integer/digit_count.cpp
@@ -0,0 +1,21 @@
+//WAP in C to count the number od digits
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, last_digit,count=0;
+    cout<<"Enter the number : ";
+    cin>>n; //287
+
+    while(n!=0)
+    {
+        last_digit = n%10; 
+        count = count + 1;
+        n = n/10;
+    }
+
+    cout<<"The number of digits is "<<count;
+
+    return 0;
+}
