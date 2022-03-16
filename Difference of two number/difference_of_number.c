78b0a2e2c7d78abcc3c56dbcd59e7f9a116015c9
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2022-03-16 23:24:05 +0530

    Add files via upload

---
+++ Difference of two number/difference_of_number.c
@@ -0,0 +1,18 @@
+//WAP to find difference between two number
+
+#include<stdio.h>
+int main()
+{
+      int a, b, sub;
+
+      printf("Enter a : ");
+      scanf("%d",&a);
+      printf("Enter b : ");
+      scanf("%d",&b);
+
+      sub = a - b;
+
+      printf("Difference = %d",sub);
+
+      return 0;
+}
---
+++ Difference of two number/difference_of_number.cpp
@@ -0,0 +1,20 @@
+//WAP to find difference between two number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+      int a, b, sub;
+
+      cout<<"Enter a : ";
+      cin>>a;
+      cout<<"Enter b : ";
+      cin>>b;
+
+      sub = a + b;
+
+      cout<<"Difference = "<<sub;
+
+      return 0;
+}
