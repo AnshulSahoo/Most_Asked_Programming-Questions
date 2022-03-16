63e60d14faff22c3401d35776eb0215ab78ed074
AnshulSahoo <70620057+AnshulSahoo@users.noreply.github.com>
2022-03-16 23:17:41 +0530

    Add files via upload

---
+++ Sum of two number/sum_of_number.cpp
@@ -0,0 +1,20 @@
+//WAP to add two number
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+      int a, b, sum;
+
+      cout<<"Enter a : ";
+      cin>>a;
+      cout<<"Enter b : ";
+      cin>>b;
+
+      sum = a + b;
+
+      cout<<"sum = "<<sum;
+
+      return 0;
+}
